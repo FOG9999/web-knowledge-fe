ee1ff1017870d28c33a3ce2ba20402406a41f15e
VIETTELGROUP\Thangnd54 <thangnd54>
2022-05-20 17:40:06 +0700

    add responsive table

--- src/shared/customs/Table.jsx
+++ src/shared/customs/Table.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import {Table} from 'antd'
+import { useSelector } from 'react-redux';
 
 const WKTable = ({
     dataSource,
@@ -10,12 +11,40 @@ const WKTable = ({
     const defaultConfig = {
         pagination: false
     }
+    const windowWidth = useSelector(state => state.window.winWidth);
 
     const additionalProps = {
         pagination: pagination?pagination:false
     }
 
-    return <Table dataSource={dataSource} columns={columns} expandable={expand} {...defaultConfig} {...additionalProps} />
+    const renderTableResponsive = () => {
+        return <table className='table'>
+            <thead>
+                {
+                    columns.map((col) => (
+                        <th scope='col' key={col.dataIndex}>
+                            <span>{col.title}</span>
+                        </th>
+                    ))
+                }
+            </thead>
+            <tbody>
+                {
+                    dataSource.map((item) => (
+                        <tr key={item.key}>
+                            {
+                                columns.map((col) => (
+                                    <td data-label={col.title} key={col.dataIndex}>{item[col.dataIndex]}</td>
+                                ))
+                            }
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+    }
+
+    return windowWidth > 1000 ? <Table dataSource={dataSource} columns={columns} expandable={expand} {...defaultConfig} {...additionalProps} scroll={{x: true, y: false}} /> : renderTableResponsive();
 }
 
 WKTable.propTypes = {
